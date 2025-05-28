e5b13a319476f18196a150b8732ad49562fb18b2
Caitlin R <raubcc@mail.uc.edu>
2025-05-28 23:46:33 +0000

    Playing with Java

---
+++ script.js/script.js
@@ -0,0 +1,6 @@
+<!-- In your HTML file, add this before </body> -->
+<script src="script.js"></script>
+<script>
+  // This is your external JavaScript file
+  alert('Hello from script.js!');
+</script>
