ffa282db5a28ae8d1867ce8578cf376b64b8d108
Caitlin R <raubcc@mail.uc.edu>
2025-05-28 23:55:04 +0000

    Remove inline JavaScript alert and link external JavaScript file for better organization

--- script.js/script.js
+++
@@ -1,6 +0,0 @@
-<!-- In your HTML file, add this before </body> -->
-<script src="script.js"></script>
-<script>
-  // This is your external JavaScript file
-  alert('Hello from script.js!');
-</script>
